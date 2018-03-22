804b7017c04d95e9982ac8214f205631074ad3df
Heitor A Neto <heitorh3@gmail.com>
2018-03-22 16:46:50 -0300

    Deixando a classe pagamento como abstrata.

--- src/main/java/br/com/heitor/cursomc/domain/Pagamento.java
+++ src/main/java/br/com/heitor/cursomc/domain/Pagamento.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToOne;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
-public class Pagamento implements Serializable {
+public abstract class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
