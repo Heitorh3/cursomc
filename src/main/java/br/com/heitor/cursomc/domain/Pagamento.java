07d67d5bdaa3e38476341db2a40d259262b52b53
Heitor A Neto <heitorh3@gmail.com>
2018-03-22 15:59:28 -0300

    Mapeamento OnoToOne

--- src/main/java/br/com/heitor/cursomc/domain/Pagamento.java
+++ src/main/java/br/com/heitor/cursomc/domain/Pagamento.java
@@ -2,23 +2,27 @@ package br.com.heitor.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 
+@Entity
 public class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Long id;
 	
 	@Enumerated(EnumType.STRING)
 	private EstadoPagamento pagamento;
 	
-	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
