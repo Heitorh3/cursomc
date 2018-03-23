550a88995e6a972c068d5246de5fc2b08c9ec226
Heitor A Neto <heitorh3@gmail.com>
2018-03-23 15:58:19 -0300

    Paginando com parametros opcionais na requisição.

--- src/main/java/br/com/heitor/cursomc/resource/CategoriaResource.java
+++ src/main/java/br/com/heitor/cursomc/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,6 +41,19 @@ public class CategoriaResource {
 		List<Categoria> categorias = service.buscarTodos();
 		
 		List<CategoriaDTO> categoriaDTO = categorias.stream().map(i -> new CategoriaDTO(i)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(categoriaDTO);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> buscarPagina(
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
+			@RequestParam(value="direction", defaultValue = "DESC")String direction){
+		Page<Categoria> categorias = service.buscarPagina(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> categoriaDTO = categorias.map(i -> new CategoriaDTO(i));
 		
 		return ResponseEntity.ok().body(categoriaDTO);
 	}
